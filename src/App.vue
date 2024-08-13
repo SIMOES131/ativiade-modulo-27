b6807dde1639c9b912301d7e94d6c6bef73252b5
Simoes Oliveira <cammom131@gmail.com>
2024-08-12 22:22:27 -0300

    Ajustando a calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,72 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, computed } from 'vue';
+
+const estado = reactive({
+  num1: 0,
+  num2: 0,
+  operacao: 'soma',
+});
+
+const resultado = computed(() => {
+  const { num1, num2, operacao } = estado;
+  switch (operacao) {
+    case 'soma':
+      return num1 + num2;
+    case 'subtracao':
+      return num1 - num2;
+    case 'multiplicacao':
+      return num1 * num2;
+    case 'divisao':
+      return num2 !== 0 ? num1 / num2 : 'Erro: Divisão por zero';
+    default:
+      return 0;
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1> Calculadora Aritmética</h1>
+  <div class="calculadora">
+    <input type="number" v-model="estado.num1" placeholder="Número 1">
+    <input type="number" v-model="estado.num2" placeholder="Número 2">
+    <select v-model="estado.operacao">
+      <option value="soma">+</option>
+      <option value="subtracao">-</option>
+      <option value="multiplicacao">*</option>
+      <option value="divisao">/</option>
+    </select>
+    
+    <p>Resultado: {{ resultado }}</p>
+  </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  font-weight: bold;
+  margin-bottom: 40px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.calculadora {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  max-width: 300px;
+  margin: auto;
+}
+input, select {
+  padding: 5px;
+  font-size: 20px;
+  text-align: center;
+  font-weight: bold;
+}
+p {
+  font-size: 25px;
+  font-weight: bold;
 }
 </style>
